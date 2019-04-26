cc0d688a32b8037566856bc2aca457e1d69f5fa6
Pezanne Khambatta <39662781+pkhambat1@users.noreply.github.com>
2019-04-26 15:51:14 +0530

    added js

---
+++ TestMvcAdminApp/wwwroot/js/UserDetails/EditRolesOfUser.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    editRolesOfUser;
+});
+
+
+
+
+
+
+
+
+
+var editRolesOfUser = $(".editRolesOfUser").click(function () {
+    $.ajax({
+        type: 'POST',
+        url: '/UserDetails/EditRolesOfUser',
+        data: userID = $('.editRolesByUser').attr('id'),
+        success: function (d) {
+            console.log(userID);
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+});
